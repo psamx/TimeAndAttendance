fd8db59ba160419e1df56e9fc45523efeda8a7d6
z3u5 <psamxg@gmail.com>
2024-03-20 15:25:52 +0200

    Added Dropdown for locations and logic for checkout/in

---
+++ timeandattendanceapp/lib/location_model.dart
@@ -0,0 +1,11 @@
+class Location {
+  final String name;
+  final double longitude;
+  final double latitude;
+
+  Location({
+    required this.name,
+    required this.longitude,
+    required this.latitude,
+  });
+}

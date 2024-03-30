9be51ff7d11876e7f9009f184b5789a8786c56ff
z3u5 <psamxg@gmail.com>
2024-03-30 17:36:06 +0200

    Added confirmation popup

--- timeandattendanceapp/lib/services/popup_service.dart
+++ timeandattendanceapp/lib/services/popup_service.dart
@@ -20,4 +20,30 @@ class PopupService {
       },
     );
   }
+  static void showConfirmationPopup(BuildContext context, String popUpTitle, String popUpText, Function onConfirm) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(popUpTitle),
+          content: Text(popUpText),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                onConfirm();
+                Navigator.of(context).pop();
+              },
+              child: const Text("Yes")
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("No")
+            ),
+          ],
+        );
+      },
+    );
+  }
 }

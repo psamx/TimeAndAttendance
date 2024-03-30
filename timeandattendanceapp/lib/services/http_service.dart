a356b84739148c8fef5cb94d1b899e94ebb4d3d3
Andreas Karamitas <Andreas.Karamitas@windsorbrokers.com>
2024-03-30 15:06:27 +0200

    created 2 service files

---
+++ timeandattendanceapp/lib/services/http_service.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'package:TimeAndAttendance/screens/login_screen.dart';
+import 'package:TimeAndAttendance/config.dart';
+
+class HttpService {
+final AppConfig _appConfig = AppConfig();
+
+
+
+}
---
+++ timeandattendanceapp/lib/services/storage_service.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class StorageService{
+  final storage = const FlutterSecureStorage();
+  
+  Future<void> saveData(String key, String value) async {
+    await storage.write(key: key, value: value);
+  }
+
+  Future<String?> readData(String key) async {
+    return await storage.read(key: key);
+  }
+
+  Future<void> deleteData(String key) async {
+    await storage.delete(key: key);
+  }
+
+  Future<void> deleteAllData() async {
+    await storage.deleteAll();
+  }
+}
